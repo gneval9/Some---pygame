4952da9eeb4533aa92d3cd132a9c2effc7589ba6
gneval9 <142158863+gneval9@users.noreply.github.com>
2024-01-11 21:37:06 +0100

    Actualizar whatsapp_bot.py

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -1,1 +1,8 @@
 #WhatsApp BOT
+import pywhatkit as kit
+
+number = "633 906 652"
+
+kit.sendwhatmsg(number, "Hola Adrian, esto es un mensaje enviado por un bot, me alegro de que estes leyendo esto :D", 12, 30)
+
+
