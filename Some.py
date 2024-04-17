257d555955013863d8336889bcd67d88aeeffbce
gneval9 <142158863+gneval9@users.noreply.github.com>
2024-04-17 22:21:38 +0200

    Update Some.py
    (17-04-2024)

--- Some.py
+++ Some.py
@@ -1,12 +1,14 @@
- #                             __ ___                           
- #  __ _ _ __   _____   ____ _| |/ _ \                          
- # / _` | '_ \ / _ \ \ / / _` | | (_) |                         
- #| (_| | | | |  __/\ V / (_| | |\__, |                         
- # \__, |_| |_|\___| \_/_\__,_|_| _/_/                          
- # |___/            / ___|  ___  / _| |___      ____ _ _ __ ___ 
- #                  \___ \ / _ \| |_| __\ \ /\ / / _` | '__/ _ \
- #                   ___) | (_) |  _| |_ \ V  V / (_| | | |  __/
- #                  |____/ \___/|_|  \__| \_/\_/ \__,_|_|  \___|
+#                                ___                           
+#  __ _ _ __   _____   ____ _| |/ _ \                          
+# / _` | '_ \ / _ \ \ / / _` | | (_) |                         
+#| (_| | | | |  __/\ V / (_| | |\__, |                         
+# \__, |_| |_|\___| \_/_\__,_|_| _/_/                          
+# |___/            / ___|  ___  / _| |___      ____ _ _ __ ___ 
+#                  \___ \ / _ \| |_| __\ \ /\ / / _` | '__/ _ \
+#                   ___) | (_) |  _| |_ \ V  V / (_| | | |  __/
+#                  |____/ \___/|_|  \__| \_/\_/ \__,_|_|  \___|
+
+#(17-04-2024)
 
 import pygame
 import random
@@ -16,17 +18,203 @@ import threading
 import sys
 
 
-
+time = 0
 
 #Definir colores
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255,0 ,0)
+RED = (255, 0 ,0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+LIGHT_RED = (255, 70, 70)
 
 player_color = RED
+
+
+#---------MENU---------#   
+
+def menu():
+
+    pygame.init()
+
+    game_over = False
+
+
+    speedup = 0.1
+
+    size = (800, 500)
+
+    pygame.mouse.set_visible(0)
+
+    #Crear ventana
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Some - By gneval9 Software')
+
+    #Reloj de FPS
+    clock = pygame.time.Clock()
+
+    # Font Setup
+    font = pygame.font.Font(None, 60)  # Selecciona la fuente y el tamaño del texto
+
+    # Renderizar texto
+    text = font.render("PLAY", True, BLACK)  # Renderiza el texto en blanco
+
+    # Posición del texto
+    text_rect = text.get_rect()
+    text_rect.center = (397, 265)
+
+    # Renderizar texto "Exit"
+    exit_text = pygame.font.Font(None, 56).render("Exit", True, BLACK)
+    exit_text_rect = exit_text.get_rect()
+    exit_text_rect.center = (397, 380)  # Modifica la posición según tu preferencia
+
+
+
+
+
+
+    #Bucle principal
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pass
+            
+            mouse_pressed = False
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_x = mouse_pos[0]
+            mouse_y = mouse_pos[1]
+            mouse_pressed = pygame.mouse.get_pressed()
+
+
+    
+        #Color de fondo
+        screen.fill(BLACK)
+
+        #---Zona de dibujo---#
+        font = pygame.font.Font(None, 140)
+        some = font.render("SOME", None, WHITE)
+        some_rect = some.get_rect(center=(396, 100))
+        screen.blit(some, some_rect)
+
+        font = pygame.font.Font(None, 30)
+        g9 = font.render("Made and developed by gneval9 Software (04-2023)", None, WHITE)
+        g9_rect = g9.get_rect(center=(400, 480))
+        screen.blit(g9, g9_rect)
+
+        square = pygame.draw.rect(screen, WHITE, (310, 210, 180, 106))
+        play_button = pygame.draw.rect(screen, GREEN, (320, 220, 160, 86))
+        screen.blit(text, text_rect)
+
+        square1 = pygame.draw.rect(screen, WHITE, (324.7, 340, 150, 76))
+        exit_button = pygame.draw.rect(screen, LIGHT_RED, (334.7, 350, 130, 56))
+        screen.blit(exit_text, exit_text_rect)
+
+        cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
+
+
+    
+        if cursor.colliderect(play_button) or cursor.colliderect(square):
+            square = pygame.draw.rect(screen, WHITE, (310, 210, 180, 106))
+            play_button = pygame.draw.rect(screen, BLUE, (320, 220, 160, 86))
+            screen.blit(text, text_rect)
+            cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
+            if mouse_pressed[0]:
+                play()
+    
+   
+
+ 
+        if cursor.colliderect(exit_button) or cursor.colliderect(square1):
+            square1 = pygame.draw.rect(screen, WHITE, (324.7, 340, 150, 76))
+            exit_button = pygame.draw.rect(screen, BLUE, (334.7, 350, 130, 56))
+            screen.blit(exit_text, exit_text_rect)
+
+            cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
+            if mouse_pressed[0]:
+                pygame.quit()
+                sys.exit()
+
+        #Actualizar pantalla
+        pygame.display.flip()
+        clock.tick(60)
+      
+       
+
+def go():
+
+    speedup = 0.1
+
+    size = (800, 500)
+
+    pygame.mouse.set_visible(0)
+
+    #Crear ventana
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Some - By gneval9 Software')
+
+    #Reloj de FPS
+    clock = pygame.time.Clock()
+
+
+    #Bucle principal
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            mouse_pressed = False
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_x = mouse_pos[0]
+            mouse_y = mouse_pos[1]
+            mouse_pressed = pygame.mouse.get_pressed()
+
+
+    
+        #Color de fondo
+        screen.fill(BLACK)
+
+        #---Zona de dibujo---#
+        font = pygame.font.Font(None, 70)
+        text = font.render("Game Over", True, RED)
+        text_rect = text.get_rect(center=(400, 200))
+        screen.blit(text, text_rect)
+
+        font = pygame.font.Font(None, 30)
+        text = font.render(f"Total time: {time}", None, WHITE)
+        text_rect = text.get_rect(center=(400, 240))
+        screen.blit(text, text_rect)
+
+        exit_text = pygame.font.Font(None, 56).render("Exit", True, BLACK)
+        exit_text_rect = exit_text.get_rect()
+        exit_text_rect.center = (397, 379)
+        square1 = pygame.draw.rect(screen, WHITE, (324, 340, 150, 76))
+        exit_button = pygame.draw.rect(screen, LIGHT_RED, (334, 350, 130, 56))
+        screen.blit(exit_text, exit_text_rect)
+        
+        
+        cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
+
+        if cursor.colliderect(exit_button) or cursor.colliderect(square1):
+            exit_text = pygame.font.Font(None, 56).render("Exit", True, BLACK)
+            exit_text_rect = exit_text.get_rect()
+            exit_text_rect.center = (397, 379)
+            square1 = pygame.draw.rect(screen, WHITE, (324, 340, 150, 76))
+            exit_button = pygame.draw.rect(screen, BLUE, (334, 350, 130, 56))
+            screen.blit(exit_text, exit_text_rect)
+            cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
+            if mouse_pressed[0]:
+                menu()
+
+
+
+        #Actualizar pantalla
+        pygame.display.flip()
+        clock.tick(60)
+
+
 
 
 
@@ -35,8 +223,6 @@ def play():
     pygame.init()
 
     game_over = False
-
-    time = 0
 
     speedup = 0.1
 
@@ -86,9 +272,9 @@ def play():
     while game_over == False:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                break
-                game_over = True
+                sys.exit()
 
+            mouse_pressed = False
             mouse_pos = pygame.mouse.get_pos()
             mouse_x = mouse_pos[0]
             mouse_y = mouse_pos[1]
@@ -189,6 +375,10 @@ def play():
         enemy3 = pygame.draw.rect(screen, RGB, (square_cord_x3, square_cord_y3, 60, 40))
         player = pygame.draw.circle(screen, player_color, (cord_x, cord_y), 30)
 
+        font = pygame.font.Font(None, 30)
+        text = font.render(f"time:", None, WHITE)
+        text_rect = text.get_rect(center=(140, 20))
+        screen.blit(text, text_rect)
 
         
         #cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
@@ -199,20 +389,6 @@ def play():
         if player.colliderect(enemy1) or player.colliderect(enemy2) or player.colliderect(enemy3):
             game_over = True
             break
-
-
-
-
-
-
-    
-            
-
-
-
-
-
-
 
         borde_arriba = pygame.draw.rect(screen, BLACK, (0, 0, 1000, 1))
         borde_abajo = pygame.draw.rect(screen, BLACK, (0, 499, 1000, 1))
@@ -238,20 +414,16 @@ def play():
                 cord_x -= 8
 
         #Actualizar pantalla
-        time += 0.0166666666
+        global time += 0.016
         pygame.display.flip()
         clock.tick(60)
-        print(time)
+    
        
 
     if game_over:
-        screen.fill(BLACK)  # Poner la pantalla en negro
+        go()
 
-        # Crear un texto "Game Over" en rojo y centrado en la pantalla
-        font = pygame.font.Font(None, 36)
-        text = font.render("Game Over", True, RED)
-        text_rect = text.get_rect(center=(400, 250))
-        screen.blit(text, text_rect)
+        
         
         
         
@@ -260,87 +432,4 @@ def play():
         print("Spawn error")
 
    
-    
-
-def menu():
-
-    pygame.init()
-
-    game_over = False
-
-    time = 0
-
-    speedup = 0.1
-
-    size = (800, 500)
-
-    pygame.mouse.set_visible(0)
-
-    #Crear ventana
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption('Some - By gneval9 Software')
-
-    #Reloj de FPS
-    clock = pygame.time.Clock()
-
-    # Font Setup
-    font = pygame.font.Font(None, 60)  # Selecciona la fuente y el tamaño del texto
-
-    # Renderizar texto
-    text = font.render("PLAY", True, BLACK)  # Renderiza el texto en blanco
-
-    # Posición del texto
-    text_rect = text.get_rect()
-    text_rect.center = (397, 313)
-
-
-
-
-
-
-    #Bucle principal
-
-    while game_over == False:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-
-            mouse_pos = pygame.mouse.get_pos()
-            mouse_x = mouse_pos[0]
-            mouse_y = mouse_pos[1]
-            mouse_pressed = pygame.mouse.get_pressed()
-
-
-    
-        #Color de fondo
-        screen.fill(BLACK)
-
-        #---Zona de dibujo---#
-        square = pygame.draw.rect(screen, WHITE, (310, 260, 180, 106))
-        play_button = pygame.draw.rect(screen, GREEN, (320, 270, 160, 86))
-        screen.blit(text, text_rect)
-        cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
-
-
-    
-        if cursor.colliderect(play_button):
-            if mouse_pressed[0]:
-                play_button = pygame.draw.rect(screen, BLUE, (320, 270, 160, 86))
-                screen.blit(text, text_rect)
-                cursor = pygame.draw.rect(screen, RED, (mouse_x, mouse_y, 20, 20))
-                play()
-    
-        #Actualizar pantalla
-        time += 0.0166666666
-        pygame.display.flip()
-        clock.tick(60)
-      
-       
-
-
 menu()
-
-
-
-
-#11-01-2024 / 15-04-2024
